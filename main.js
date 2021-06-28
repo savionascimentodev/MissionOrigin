3f1e3d9d80bf4847030a629976d05c4ad47cc5f0
Savio Nascimento <savionascimentodev@gmail.com>
2021-06-28 03:19:08 -0300

    ✨ class 02 - expanding knowledge

---
+++ main.js
@@ -0,0 +1,32 @@
+// Abre e fecha menu quanco clicar no icone: hambuguer e
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// Quando clicar em um item do menu, esconder o menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// Mudar header da página quando der scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll maior que altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
